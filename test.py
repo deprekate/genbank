b863e266e73b61b93d5fdeedf9f0384b93945a7b
deprekate <deprekate@gmail.com>
2022-02-15 15:32:40 -0800

    Added demo script

--- test.py
+++ test.py
@@ -3,4 +3,7 @@ import sys
 from genbank.file import File
 
 f = File(sys.argv[1])
-print(f)
+for name,locus in f.items():
+	print("-----Locus:",name, "-----")
+	for feature in locus:
+		print(feature)
