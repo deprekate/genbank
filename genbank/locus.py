00ec740ed79ed228981ef297075d5f89f69479fd
deprekate <deprekate@gmail.com>
2022-11-30 13:21:26 -0800

    Forgot revcomp

--- genbank/locus.py
+++ genbank/locus.py
@@ -254,14 +254,14 @@ class Locus(dict):
 
 	def codon_rarity(self, codon=None):
 		if not hasattr(self, 'rarity'):
-			seen = dict()
+			seen = {-1:dict(), +1:dict()}
 			self.rarity = {a+b+c : 1 for a in 'acgt' for b in 'acgt' for c in 'acgt'}
 			for feature in self:
 				if feature.type == 'CDS':
 					for _codon, _loc in zip(feature.codons(), feature.codon_locations()):
-						if _codon in self.rarity and _loc not in seen:
+						if _codon in self.rarity and _loc not in seen[feature.strand]:
 							self.rarity[_codon] += 1
-							seen[_loc] = True
+							seen[feature.strand][_loc] = True
 		total = sum(self.rarity.values())
 		self.rarity = {codon:self.rarity[codon]/total for codon in self.rarity}
 		if codon in self.rarity:
